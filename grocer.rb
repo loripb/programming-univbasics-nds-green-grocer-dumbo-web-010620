b27a7a5471bb50ee9d178b21b974fe6d93968b5d
Lori Boyd <lori.p.boyd@gmail.com>
2019-12-25 05:41:43 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -113,6 +113,9 @@ def checkout(cart, coupons)
   cart = consolidate_cart(cart)
   cart = apply_coupons(cart, coupons)
   cart = apply_clearance(cart)
-  p cart
+  
   total = find_cart_total(cart)
+  if total > 100 
+    puts 'hey'
+  end
 end
